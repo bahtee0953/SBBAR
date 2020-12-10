4c4e3d8b2863a40085c1e38abeb748c08948431f
bahtee0953 <74145085+bahtee0953@users.noreply.github.com>
2020-12-10 15:31:00 +0600

    SBBAR

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var map;
+
+// DG.then(function () {
+//     map = DG.map('map', {
+//         center: [],
+//         zoom: 13
+//     });
+// });
+
+var map;
+DG.then(function () {
+    map = DG.map('map', {
+        center: [42.87, 74.59],
+        zoom: 13
+    });
+});
